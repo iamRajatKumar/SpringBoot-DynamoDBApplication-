0662b40cc8a896ec213c59a287b2e706ec0a5def
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-07-04 16:38:14 +0530

    Update and Delete Functionality

--- src/main/java/com/example/DynamoDBApplication/controller/PersonController.java
+++ src/main/java/com/example/DynamoDBApplication/controller/PersonController.java
@@ -1,23 +1,24 @@
 package com.example.DynamoDBApplication.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.DynamoDBApplication.entity.Person;
 import com.example.DynamoDBApplication.repository.PersonRepository;
 
-// import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @RestController
-// @RequiredArgsConstructor
+@RequiredArgsConstructor
 public class PersonController {
 
-    @Autowired
-    public PersonRepository personRepository;
+    // @Autowired
+    public final PersonRepository personRepository;
 
     @PostMapping("/person")
     public Person savePerson(@RequestBody Person person){
@@ -26,8 +27,28 @@ public class PersonController {
 
     @GetMapping("/person/{id}")
     public Person getPerson(@PathVariable("id") String personId){
-        return personRepository.getPerson(personId);
+        return personRepository.getPersonById(personId);
     }
 
+    @PutMapping("/person/{id}")
+    public Person updatePerson(@PathVariable String id, @RequestBody Person updatedPerson){
+        Person existingPerson= personRepository.getPersonById(id);
+        if(existingPerson != null) {
+        //update code
+        existingPerson.setName(updatedPerson.getName());
+        existingPerson.setEmail(updatedPerson.getEmail());
+        existingPerson.setAddress(updatedPerson.getAddress());
+
+        return personRepository.addPerson(existingPerson);
+        }
+        return null;
+    }
+
+    //Delete
+    @DeleteMapping("/person/{id}")
+    public String deletePerson(@PathVariable String id){
+        personRepository.deletePerson(id);
+        return "Person Deleted Successfully: "+id;
+    }
 
 }
--- src/main/java/com/example/DynamoDBApplication/repository/PersonRepository.java
+++ src/main/java/com/example/DynamoDBApplication/repository/PersonRepository.java
@@ -1,14 +1,8 @@
 package com.example.DynamoDBApplication.repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 import com.example.DynamoDBApplication.entity.Person;
 import lombok.RequiredArgsConstructor;
 
@@ -20,31 +14,42 @@ public class PersonRepository {
     // final
     private final DynamoDBMapper mapper;
 
+    // Add or Update Person Details
     public Person addPerson(Person person) {
         mapper.save(person);
         return person;
     }
 
-    public Person getPerson(String personId) {
+    public Person getPersonById(String personId) {
         return mapper.load(Person.class, personId);
     }
 
-    public String deletePerson(Person person) {
-        mapper.delete(person);
+    // public Person getAllPerson(Person person) {
+    // return mapper.load(person);
+    // }
+
+    public String deletePerson(String personId) {
+        Person person = mapper.load(Person.class, personId);
+        if(person != null){
+            mapper.delete(person);
+        }
+        
         return "person deleted";
     }
 
-    public String editPerson(Person person) {
-        mapper.save(person, buildExpression(person));
-        return "Person Modified";
-    }
+    // public String editPerson(Person person) {
+    // mapper.save(person, buildExpression(person));
+    // return "Person Modified";
+    // }
 
-    private DynamoDBSaveExpression buildExpression(Person person) {
-        DynamoDBSaveExpression dynamoDBSaveExpression = new DynamoDBSaveExpression();
-        Map<String, ExpectedAttributeValue> expectedAttributeValueMap = new HashMap<>();
-        expectedAttributeValueMap.put("personId", new ExpectedAttributeValue(
-                new AttributeValue().withS(person.getEmail())));
-        dynamoDBSaveExpression.setExpected(expectedAttributeValueMap);
-        return dynamoDBSaveExpression;
-    }
+    // private DynamoDBSaveExpression buildExpression(Person person) {
+    // DynamoDBSaveExpression dynamoDBSaveExpression = new DynamoDBSaveExpression();
+    // Map<String, ExpectedAttributeValue> expectedAttributeValueMap = new
+    // HashMap<>();
+    // expectedAttributeValueMap.put("personId", new ExpectedAttributeValue(
+    // new AttributeValue().withS(person.getEmail())));
+    // dynamoDBSaveExpression.setExpected(expectedAttributeValueMap);
+    // return dynamoDBSaveExpression;
+    // }
+
 }
