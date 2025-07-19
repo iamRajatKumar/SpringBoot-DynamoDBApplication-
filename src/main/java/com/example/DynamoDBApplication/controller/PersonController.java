7f500c6ba615f78ae2c5b14a059b4ee475d7164e
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-07-19 22:02:47 +0530

    minor changes in controller

--- src/main/java/com/example/DynamoDBApplication/controller/PersonController.java
+++ src/main/java/com/example/DynamoDBApplication/controller/PersonController.java
@@ -2,6 +2,7 @@ package com.example.DynamoDBApplication.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,45 +17,52 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin("*") //connected with angular
+@CrossOrigin("*") // connected with angular
 public class PersonController {
 
     // @Autowired
     public final PersonRepository personRepository;
 
     @PostMapping("/person")
-    public Person savePerson(@RequestBody Person person){
+    public Person savePerson(@RequestBody Person person) {
         return personRepository.addPerson(person);
     }
 
     @GetMapping("/person")
-    public List<Person> getAllPersons(){
+    public List<Person> getAllPersons() {
         return personRepository.getAllPersons();
     }
 
     @GetMapping("/person/{id}")
-    public Person getPerson(@PathVariable("id") String personId){
+    public Person getPerson(@PathVariable("id") String personId) {
         return personRepository.getPersonById(personId);
     }
 
     @PutMapping("/person/{id}")
-    public Person updatePerson(@PathVariable String id, @RequestBody Person updatedPerson){
-        Person existingPerson= personRepository.getPersonById(id);
-            if(existingPerson != null) {
-            //update code
+    public Person updatePerson(@PathVariable String id, @RequestBody Person updatedPerson) {
+        Person existingPerson = personRepository.getPersonById(id);
+        if (existingPerson != null) {
+            // update code
             existingPerson.setName(updatedPerson.getName());
             existingPerson.setEmail(updatedPerson.getEmail());
             existingPerson.setAddress(updatedPerson.getAddress());
             return personRepository.addPerson(existingPerson);
-            }
+        }
         return null;
     }
 
-    //Delete
+    // Delete code for postman
+    // @DeleteMapping("/person/{id}")
+    // public String deletePerson(@PathVariable String id){
+    // personRepository.deletePerson(id);
+    // return "Person Deleted Successfully: "+id;
+    // }
+
+    // delete code for Angular error fix HttpErrorResponse 
     @DeleteMapping("/person/{id}")
-    public String deletePerson(@PathVariable String id){
+    public ResponseEntity<String> deletePerson(@PathVariable String id) {
         personRepository.deletePerson(id);
-        return "Person Deleted Successfully: "+id;
+        return ResponseEntity.ok("Person Deleted Successfully: " + id);
     }
 
 }
